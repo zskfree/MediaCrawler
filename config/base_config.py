1476b6e421a6a90553202dd488e7fb5b8602374b
zskfree <2300123009@email.szu.edu.cn>
2025-07-18 17:49:09 +0800

    更新视频爬取数量

--- config/base_config.py
+++ config/base_config.py
@@ -84,7 +84,7 @@ USER_DATA_DIR = "%s_user_data_dir"  # %s will be replaced by platform name
 START_PAGE = 1
 
 # 爬取视频/帖子的数量控制
-CRAWLER_MAX_NOTES_COUNT = 200
+CRAWLER_MAX_NOTES_COUNT = 400
 
 # 并发爬虫数量控制
 MAX_CONCURRENCY_NUM = 1
@@ -96,7 +96,7 @@ ENABLE_GET_IMAGES = False
 ENABLE_GET_COMMENTS = True
 
 # 爬取一级评论的数量控制(单视频/帖子)
-CRAWLER_MAX_COMMENTS_COUNT_SINGLENOTES = 10
+CRAWLER_MAX_COMMENTS_COUNT_SINGLENOTES = 1000
 
 # 是否开启爬二级评论模式, 默认不开启爬二级评论
 # 老版本项目使用了 db, 则需参考 schema/tables.sql line 287 增加表字段
